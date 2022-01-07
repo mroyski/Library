2de140125ac12aa9a8fc4f2e391e58d8da3adfe2
Mike Royski <mroyski@gmail.com>
2022-01-07 15:00:14 -0500

    fixing names of files/classes

--- Library.Tests/DbSetBuilder.cs
+++ Library.Tests/DbSetBuilder.cs
@@ -1,43 +1,25 @@
+﻿using Library.Shared;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Library.Api.Data;
-using Library.Shared;
-using Microsoft.EntityFrameworkCore;
-using Moq;
-using Xunit;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Library.Tests
 {
-    public class DbSetBuilder
+    internal static class DbSetBuilder<T> where T : class
     {
-        private readonly Mock<DbSet<Reserve>> mockSet;
-        private readonly Mock<LibraryContext> mockContext;
-        private readonly ReservesRepository mockRepo;
-        public DbSetBuilder()
+        public static Mock<DbSet<T>> Build(IQueryable<T> data)
         {
-            var data = new List<Reserve>
-            {
-                new Reserve { ReserveId = 1, BookId = 1, MemberId = 1, ReserveDate = DateTime.Now, ReserveStatus = "pending" }
-            }.AsQueryable();
+            var mockSet = new Mock<DbSet<T>>();
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
 
-            mockSet = DbSetBuilder<Reserve>.Build(data);
-            mockContext = ContextBuilder.BuildReserveContext(mockSet);
-            mockRepo = new ReservesRepository(mockContext.Object);
-        }
-
-        [Fact]
-        public void GetReservation_ReturnsReservationById()
-        {
-            var reserve = mockRepo.GetReservation(1);
-
-            Assert.Equal(1, reserve.ReserveId);
-        }
-
-        [Fact]
-        public void MemberHasExistingReservation_ThrowsException()
-        {
-            Assert.Throws<Exception>(() => mockRepo.AddReservation(1, 1));
+            return mockSet;
         }
     }
 }
---
+++ Library.Tests/ReserveTests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library.Api.Data;
+using Library.Shared;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace Library.Tests
+{
+    public class ReserveTests
+    {
+        private readonly Mock<DbSet<Reserve>> mockSet;
+        private readonly Mock<LibraryContext> mockContext;
+        private readonly ReservesRepository mockRepo;
+        public ReserveTests()
+        {
+            var data = new List<Reserve>
+            {
+                new Reserve { ReserveId = 1, BookId = 1, MemberId = 1, ReserveDate = DateTime.Now, ReserveStatus = "pending" }
+            }.AsQueryable();
+
+            mockSet = DbSetBuilder<Reserve>.Build(data);
+            mockContext = ContextBuilder.BuildReserveContext(mockSet);
+            mockRepo = new ReservesRepository(mockContext.Object);
+        }
+
+        [Fact]
+        public void GetReservation_ReturnsReservationById()
+        {
+            var reserve = mockRepo.GetReservation(1);
+
+            Assert.Equal(1, reserve.ReserveId);
+        }
+
+        [Fact]
+        public void MemberHasExistingReservation_ThrowsException()
+        {
+            Assert.Throws<Exception>(() => mockRepo.AddReservation(1, 1));
+        }
+    }
+}
